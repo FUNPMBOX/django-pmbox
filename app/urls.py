18f1f45e57049ce92231f047bec9a3451fc28a9d
Adrian Castellanos <adriancast@MacBook-Pro-de-Adrian.local>
2018-02-12 20:23:22 +0100

    Now game model has an image
    Image added to the model, now if a game has an image it
    displays it in result view and detail view.

--- app/urls.py
+++ app/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls import url, include
+from django.conf.urls.static import static
+from games.views import home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url(r'games', include('games.urls')),
+    url(r'^$', home, name='home')
 ]
+
+# This is required to serve media files in debug mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
